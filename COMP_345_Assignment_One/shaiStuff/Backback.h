049b0f21637dec820708ef78952e911986c99812
Shai Troy <148577433+shaiTroy@users.noreply.github.com>
2024-02-23 17:07:53 -0500

    all the files

---
+++ COMP_345_Assignment_One/shaiStuff/Backback.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include "Item.h"
+
+using namespace std;
+
+class Backback : public Item{
+private:
+   int space;
+   vector<Item> contents;
+public:
+    Backback(bool equippedValue, int spc) : Item(equippedValue), space(spc){};
+    Backback(int spc) : space(spc){};
+    Backback();
+    ~Backback();
+
+    int getSpace(){return space;};
+    void setSpace(int value){space = value;};
+
+    vector<Item> getContents(){return contents;};
+    void addContent(Item itm){contents.push_back(itm);};
+};
+
+
