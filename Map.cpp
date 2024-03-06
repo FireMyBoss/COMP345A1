29194be21574702c1cef05e37d393928459282e6
Nicholas Kamra <99359072+yckamra@users.noreply.github.com>
2024-03-06 18:48:38 -0500

    Add files via upload

---
+++ Map.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Nicholas Kamra on 3/6/24.
+//
+
+#include "Map.h"
+
+Map::Map(int height, int width){
+    this->height = height;
+    this->width = width;
+
+    for(int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            std::vector<Cell *> newVector;
+            map.push_back(newVector);
+            Cell * newCell = new Cell();
+            map.at(i).push_back(newCell);
+        }
+    }
+
+}
+void Map::to_string(){
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++) {
+
+            std::cout << this->map.at(i).at(j)->state.letter;
+
+        }
+        std::cout << "" << std::endl;
+    }
+}
+void Map::createStart(){}
---
+++ Map.h
@@ -0,0 +1,23 @@
+//
+// Created by Nicholas Kamra on 3/6/24.
+//
+
+#ifndef SOURCE_FILES_TRIAL_3_MAP_H
+#define SOURCE_FILES_TRIAL_3_MAP_H
+
+#include "Cell.h"
+#include <vector>
+#include <iostream>
+class Map {
+public:
+    int height;
+    int width;
+    std::vector<std::vector<Cell *>> map;
+
+    Map(int height, int width);
+    void to_string();
+
+};
+
+
+#endif //SOURCE_FILES_TRIAL_3_MAP_H
