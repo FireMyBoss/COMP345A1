8e776ce696bd13c9d86ca409cd3716a5cc89435c
shaiTroy <shaitroy6@gmail.com>
2024-03-07 16:34:01 -0500

    treasure chest

--- Source_Files_Trial_3/TreasureChest.h
+++ Source_Files_Trial_3/TreasureChest.h
@@ -13,10 +13,12 @@ class TreasureChest : public Item, public State
 private:
     int space;
     vector<Item> contents;
+    char letter;
+    string colour;
 public:
-    TreasureChest(int spc, vector<Item> & preExistingContent) : space(spc), contents(preExistingContent){this->letter = 'T'; this->colour = "yellow";};
-    explicit TreasureChest(vector<Item> & preExistingContent) : contents(preExistingContent){this->letter = 'T'; this->colour = "yellow";};
-    explicit TreasureChest(int spc) : space(spc){this->letter = 'T'; this->colour = "yellow";};
+    TreasureChest(int spc, vector<Item> & preExistingContent) : space(spc), contents(preExistingContent){this->letter = 'C'; this->colour = "orange";};
+    explicit TreasureChest(vector<Item> & preExistingContent) : contents(preExistingContent){this->letter = 'C'; this->colour = "orange";};
+    explicit TreasureChest(int spc) : space(spc){this->letter = 'C'; this->colour = "orange";};
 
     int getSpace() const {return space;};
     void setSpace(int value){space = value;};
