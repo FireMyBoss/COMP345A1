77831088930711fe0d3d672fb74ee590f3dadcba
FireMyBoss <103454069+FireMyBoss@users.noreply.github.com>
2024-03-09 05:37:18 +0000

    Revamped Dice and misc
    Made the dice act more closely to what Taleb is asking, as well as misc

--- Source_Files_Trial_3/Dice.cpp
+++ Source_Files_Trial_3/Dice.cpp
@@ -2,28 +2,371 @@
 // Created by Nicholas Kamra on 3/6/24.
 //
 
-#include "Dice.h"
-int Dice::roll(const std::string & expression) {
-    std::istringstream iss(expression);
-    char d;
-    int numDice, sides, modifier;
-    iss >> numDice >> d >> sides;
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+#include "misc.h"
+//#include "userInput.h"
+
+using namespace std;
+
+/**
+ * @class Dice
+ * @brief Class to represent a dice and perform dice rolling operations.
+ */
+class Dice {
+
+	public:
+	
+		int numOfDice; //Number of Dice to roll
+		int numOfSidesDie; //Number of sides on dice
+		int modifyValue; //What should the final value of the dice output be modified by
+		char* diceValue; //String that holds dice info
+		
+		/**
+     * @brief Default constructor for Dice class.
+     */
+		Dice() {
+		
+			numOfDice = 0;
+			numOfSidesDie = 0;
+			modifyValue = 0;
+			diceValue = nullptr;
+		
+		}
+		
+		/**
+     * @brief Parameterized constructor for Dice class.
+     * @param numOfDice Number of dice to set
+     * @param numOfSidesDie Number of sides on each die to set
+     * @param modifyValue Value to modify the final output by
+     */
+		Dice(int numOfDice, int numOfSidesDie, int modifyValue) {
+			
+			this -> numOfDice = numOfDice;
+			this -> numOfSidesDie = numOfSidesDie;
+			this -> modifyValue = modifyValue;
+			this -> diceValue = constructDiceValue(); //Special method that constructs the dice string value
+			
+		}
+		
+		/**
+     * @brief Destructor for Dice class.
+     */
+		~Dice() {
+		
+		  if (diceValue != nullptr) {
+		  
+		      delete[] diceValue;
+		      diceValue = nullptr;
+		      
+		  }
+		  
+		}
+		
+		/**
+     * @brief Get the number of dice.
+     * @return The number of dice.
+     */
+		int getNumOfDice() {
+		
+			return numOfDice;
+		
+		}
+		
+		 /**
+     * @brief Set the number of dice.
+     * @param numOfDice The number of dice to set.
+     */
+		void setNumOfDice(int numOfDice) {
+		
+			this -> numOfDice = numOfDice;
+			this -> diceValue = constructDiceValue();
+		
+		}
+		
+		/**
+     * @brief Get the number of sides on each die.
+     * @return The number of sides on each die.
+     */
+		int getNumOfSidesDie() {
+		
+			return numOfSidesDie;
+		
+		}
+		
+		/**
+     * @brief Set the number of sides on each die.
+     * @param numOfSidesDie The number of sides on each die to set.
+     */
+		void setNumOfSidesDie(int numOfSidesDie) {
+			
+			this -> numOfSidesDie = numOfSidesDie;
+			this -> diceValue = constructDiceValue();
+		
+		}
+		
+		/**
+     * @brief Get the modification value for the final output.
+     * @return The modification value.
+     */
+		int getModifyValue() {
+		
+			return modifyValue;
+		
+		}
+		
+		 /**
+     * @brief Set the modification value for the final output.
+     * @param modifyValue The modification value to set.
+     */
+		void setModifyValue(int modifyValue) {
+		
+			this -> modifyValue = modifyValue;
+			this -> diceValue = constructDiceValue();
+		
+		}
+		
+		/**
+     * @brief Get the string representation of the dice information.
+     * @return The string representing the dice information.
+     */
+		char* getDiceValue() {
+		
+			return diceValue;
+		
+		}
+		
+		/**
+     * @brief Set the dice value based on a given string input.
+     * @param diceValue The string input representing the new dice value.
+     */
+		void setDiceValue(char* diceValue) {
+			
+			if(deconstructDiceValue(diceValue) == 1) { //Make sure that the inputted dice string is properly formatted
+			
+				delete[] this -> diceValue;
+				this -> diceValue = constructDiceValue();
+			
+			}
+
+		}
+		
+		/**
+     * @brief Roll the dice and return the result based on current settings.
+     * @return The result of rolling the dice with modifications applied.
+     */
+		int roll() {
+			
+			int retVal = 0;
+			
+			for(int i = 0; i < numOfDice; i++) {
+			
+				retVal += (getCurrentTime() % numOfSidesDie) + 1;
+			
+			}
+		
+			return retVal + modifyValue;
+		
+		}
+		
+		/**
+     * @brief Roll the dice based on a given input string and return the result.
+     * @param inputDiceString The input string representing the new dice value to roll with.
+     * @return The result of rolling the modified dice based on input string.
+     */
+		int roll(char* inputDiceString) {
+		
+			if(deconstructDiceValue(inputDiceString) == 0) { //Ensures that the inputted dice string is valid, otherwise it returns nothing at all
+			
+				return 0;
+			
+			}
+			
+			return roll();
+		
+		}
+		
+	private:
+		
+		/**
+		* @brief Construct a string representation of the current dice settings.
+		* @return A dynamically allocated char array representing the current dice settings as a string.
+		*/
+		char* constructDiceValue() {
+	
+			int digitsNumOfDice = getNumOfDigitsInInt(numOfDice,10); //Get the number of digits that represents the number of dice 
+			int digitsNumOfSidesDie = getNumOfDigitsInInt(numOfSidesDie, 10); //Get the number of digits that represents the number of sides per die
+			int digitsModifyValue = getNumOfDigitsInInt(modifyValue, 10); //Get the number of digits that represent the modification value when finished rolling
+			
+			char* diceValue = new char[digitsNumOfDice + 1 + digitsNumOfSidesDie + 1 + digitsModifyValue + 1]; //The first +1 is for the d, the second is for the + or - sign, the third is for the null terminator
+			
+			int currentIndexDiceVal = 0; //Determines what index to place the dice characters at int the return string
+			
+			for(int i = 0; i < digitsNumOfDice; i++) { //Handle all Number of Dice digits for the return string
+			
+				int tempDigit = getNumberAtDigit(numOfDice, 10, i + 1); //Gets the (ith + 1) (so 1st to digitsNumOfDice + 1th) digit of numOfDice
+				diceValue[currentIndexDiceVal] = (char) (tempDigit + 48); //48 is the ASCII code for the character '0'
+				
+				currentIndexDiceVal++;
+			
+			}
+			
+			diceValue[currentIndexDiceVal] = 'd'; //Sets the 'd' delimiter between number of dice and sides per die
+			currentIndexDiceVal++;
+			
+			for(int i = 0; i < digitsNumOfSidesDie; i++) { //Handle all Number of Sides per die digits for the return string
+			
+				int tempDigit = getNumberAtDigit(numOfSidesDie, 10, i + 1); //Gets the (ith + 1) (so 1st to digitsNumOfDice + 1th) digit of numOfSidesDie
+				diceValue[currentIndexDiceVal] = (char) (tempDigit + 48); //48 is the ASCII code for the character '0'
+				
+				currentIndexDiceVal++;
+			
+			}
+			
+			diceValue[currentIndexDiceVal] = modifyValue > 0 ? '+' : '-'; //Sets the parity character (+ or -) of modifyValue 
+			currentIndexDiceVal++;
+			
+			for(int i = 0; i < digitsModifyValue; i++) { //Handle all of the modified amount after roll per die digits for the return string
+			
+				int tempDigit = getNumberAtDigit(modifyValue, 10, i + 1); //Gets the (ith + 1) (so 1st to digitsNumOfDice + 1th) digit of modifyValue
+				diceValue[currentIndexDiceVal] = (char) (tempDigit + 48); //48 is the ASCII code for the character '0'
+				
+				currentIndexDiceVal++;
+			
+			}
+			
+			diceValue[currentIndexDiceVal] = '\0'; //Finish string with null terminator
+			
+		return diceValue;
+	
+	}
+	
+	/**
+	* @brief Deconstruct a given string into individual components and update current settings accordingly.
+	* @param diceVal The input string representing a new set of dice settings to deconstruct and apply.
+	* @return 0 if successful, 1 if unsuccessful in deconstruction process.
+	*/
+	int deconstructDiceValue(const char* diceVal) {
+			
+			//Initialize amount of digits for each section (# Dice, # Sides per Dice, Modification value) of the string
+			int digitsNumOfDice = 0;
+			int digitsNumOfSidesDie = 0;
+			int digitsModifyValue = 0;
+			
+			//Initialize integer values for each section (# Dice, # Sides per Dice, Modification value) of the string
+			int numOfDice = 0;
+			int numOfSidesDie = 0;
+			int modifyValue = 0;
+			
+			int negateModifyValue = 0; //Flag to check if modifyValue should be positive or negative
+			
+			int currentIndexDiceVal= 0; //Determines what index to keep track of the dice characters in the inputted dice string
+			
+			while(diceVal[currentIndexDiceVal] != '\0' && diceVal[currentIndexDiceVal] >= '0' && diceVal[currentIndexDiceVal] <= '9') { //Check if number of dice exist
+				
+				digitsNumOfDice++;
+				currentIndexDiceVal++;
+				
+			}
+			
+			if(digitsNumOfDice == 0) { //If there are no digits in the numOfDie number, return 0
+			
+				return 0;
+			
+			}
+			
+			for(int i = digitsNumOfDice; i > 0; i--) { //Calcuate the number of dice (Number at digit * 10^(Number of Digits - Specific Digit - 1))
+				
+				//Ex: The substring, "1234" becomes: 1*10^3 + 2*10^2 + 3*10^1 + 4*10^0
+				numOfDice += (diceVal[currentIndexDiceVal - i] - 48) * pow(10, i - 1); //48 is the ASCII code for the character '0'
+			
+			}
+			
+			
+			if (diceVal[currentIndexDiceVal] != 'd') { //Check to see if the d character is present
+					
+				return 0; // Return from the function if 'd' character is not present
+			
+			} else {
+				
+				currentIndexDiceVal++; // Increment currentIndexDiceVal if 'd' character is present
+			
+			}
+			
+			
+   		while(diceVal[currentIndexDiceVal] != '\0' && diceVal[currentIndexDiceVal] >= '0' && diceVal[currentIndexDiceVal] <= '9') { //Check if number of sides per die exist
+				
+				digitsNumOfSidesDie++;
+				currentIndexDiceVal++;
+				
+			}
+			
+			if(digitsNumOfSidesDie == 0) { //If there are no digits in the number of sides in the dice, return 0
+			
+				return 0;
+			
+			}
+			
+			for(int i = digitsNumOfSidesDie; i > 0; i--) { //Calcuate the number of sides per die (Number at digit * 10^(Number of Digits - Specific Digit - 1))
+				
+				//Ex: The substring, "1234" becomes: 1*10^3 + 2*10^2 + 3*10^1 + 4*10^0
+				numOfSidesDie += (diceVal[currentIndexDiceVal - i] - 48) * pow(10, i - 1); //48 is the ASCII code for the character '0'
+			
+			}
+			
+			
+			if (diceVal[currentIndexDiceVal] == '+') { //Check to see if the '+' character is present
+			
+				currentIndexDiceVal++; // Increment currentIndexDiceVal if '+' character is present
+			
+			} else if(diceVal[currentIndexDiceVal] == '-') { //Check to see if the '-' character is present
+				
+				currentIndexDiceVal++; // Increment currentIndexDiceVal if '+' character is present
+				negateModifyValue = 1; // Set the boolean flag to make modifyValue negative
+			
+			} else {
+				
+				return 0; // Return from the function if '+' or '-' character is not present
+	
+			}
+			
+			
+			while(diceVal[currentIndexDiceVal] != '\0' && diceVal[currentIndexDiceVal] >= '0' && diceVal[currentIndexDiceVal] <= '9') { //Check if the modification number exists for the dice
+				
+				digitsModifyValue++;
+				currentIndexDiceVal++;
+				
+			}
+			
+			
+			for(int i = digitsModifyValue; i > 0; i--) { //Calcuate the number of sides per die (Number at digit * 10^(Number of Digits - Specific Digit - 1))
+				//Ex: The substring, "1234" becomes: 1*10^3 + 2*10^2 + 3*10^1 + 4*10^0
+				modifyValue += (diceVal[currentIndexDiceVal - i] - 48) * pow(10, i - 1); //48 is the ASCII code for the character '0'
+			
+			}
+			
+			if(negateModifyValue) { //Checks to see if modifyValue should be made negative or not (based on if + or - was present in the string)
+			
+				modifyValue = -1*modifyValue;
+			
+			}
+			
+			//Set the calculated values of each substring section to the dice
+			this -> numOfDice = numOfDice;
+			this -> numOfSidesDie = numOfSidesDie;
+			this -> modifyValue = modifyValue;
+			
+			return 1;
+   		
+		}
+
+};
 
 
-    if (iss.peek() == '+' || iss.peek() == '-') {
-        iss >> modifier;
-    }
-    else {
-        modifier = 0; // Default
-    }
-
-    int total = 0;
-    for (int i = 0; i < numDice; ++i) {
-        total += rollDie(sides);
-    }
-
-    return total + modifier;
-}
-int Dice::rollDie(int sides) {
-    return rand() % sides + 1;
-}
--- Source_Files_Trial_3/misc.h
+++ Source_Files_Trial_3/misc.h
@@ -1,13 +1,159 @@
 #pragma once
 
-long long getCurrentTime();
+using namespace std;
 
-int writeToFile(const char* filename, const char* content);
+/**
+ * @brief Function to get the current time in nanoseconds.
+ * 
+ * This function retrieves the current time using CLOCK_REALTIME and calculates the current time in nanoseconds.
+ * 
+ * @return The current time in nanoseconds.
+ */
+long long getCurrentTime() {
 
-void openNewTerminalWindow(int width, int height);
+    struct timespec current_time; // Create a struct to hold the current time information
 
-void pause(unsigned int milliseconds);
+    clock_gettime(CLOCK_REALTIME, &current_time); // Retrieve the current time using CLOCK_REALTIME and store it in the 'current_time' struct
 
-void clearConsole();
+		// Calculate the current time in nanoseconds by converting seconds to nanoseconds and adding the current nanosecond time.
+    long long int time_in_ns = current_time.tv_sec * 1000000000LL + current_time.tv_nsec; 
 
-void clearInputBuffer();
+    return(time_in_ns); // Return the current time in nanoseconds
+    
+}
+
+/**
+ * @brief Function to write content to a file.
+ * 
+ * This function writes the provided content to a file specified by the filename.
+ * 
+ * @param filename The name of the file to write to.
+ * @param content The content to write to the file.
+ * @return 0 if successful, -1 if content is null, -2 if file cannot be opened, -3 if an error occurs with the file stream, -4 if an error occurs while closing the file.
+ */
+int writeToFile(const char* filename, const char* content) {
+		
+		if(!content) { // If the input is null
+		
+			return -1;
+		
+		}
+		
+    std::ofstream outputFile(filename); // Open the file in output mode, and if it doesn't exist, create it
+
+    if (!outputFile.is_open()) { // If the file can't be opened
+
+        return -2;
+    
+    } else if (!outputFile.good()) { // If something went wrong with the file stream
+
+        return -3;
+        
+    }
+
+    outputFile << content; // Write the content to the file
+
+    outputFile.close(); // Close the file
+    
+		if (!outputFile.good()) { // If something went wrong while closing the file
+     
+	    return -4;
+
+		}
+    
+    return 0;
+    
+}
+
+/**
+ * @brief Function to pause execution for a specified number of milliseconds.
+ * 
+ * This function pauses execution for a specified number of milliseconds using usleep.
+ * 
+ * @param milliseconds The number of milliseconds to pause for.
+ */
+void pause(unsigned int milliseconds) {
+
+    usleep(milliseconds * 1000); 
+    
+}
+
+/**
+ * @brief Function to clear the console screen.
+ */
+void clearConsole() {
+  
+  cout << "\033[H\033[2J\033[3J"; //Magic string that clears the console screen
+  
+}
+
+/**
+ * @brief Function to get the number of digits in an integer for a given radix.
+ * 
+ * This function calculates and returns the number of digits in an integer for a given radix.
+ * 
+ * @param input The integer input value.
+ * @param radix The radix for which digits are counted.
+ * @return The number of digits in the integer for the specified radix.
+ */
+int getNumOfDigitsInInt(int input, int radix) {
+
+	if(input == 0) { //0 has one digit, since it's still a number
+	
+		return 1;
+	
+	}
+
+	int retNumOfDigits = 0;
+	
+	while(input != 0) { //Keep diving by radix until the number gets rounded down/up to zero
+	
+		input = (int) floor(input/radix);
+		retNumOfDigits++;
+	
+	}
+	
+	return retNumOfDigits;
+	
+}
+
+/**
+ * @brief Function to get a digit at a specific index from an integer for a given radix.
+ *
+ * This function retrieves and returns a digit at a specific index from an integer for a given radix.
+ *
+ * @param input The integer input value.
+ * @param radix The radix for which digits are counted.
+ * @param desiredIndex The index of the digit to retrieve (starting from 1).
+ *
+ * @return The digit at the desired index. Returns -1 for invalid index or input values.
+ */
+int getNumberAtDigit(int input, int radix, int desiredIndex) {
+	
+	//Ex: The 1st index of 9850 is 9, 2nd is 8, etc.
+	
+	if(input == 0) {
+	
+		return 0;
+	
+	} else if (input < 0) {
+	
+		input = -input;
+	
+	}
+	
+	int numDigits = getNumOfDigitsInInt(input, radix);
+	
+	int divisor = pow(radix, numDigits - desiredIndex); //Get the required divisor in order to make the input / divisor always less than the radix 
+	
+	if(desiredIndex < 0 || input < divisor) { //Checks to see if an invalid index is trying to be acessed, if the divisor is larger than the input, or if the input is 0
+	
+		return -1;
+	
+	}
+	
+	int retVal = (int) ((input/divisor) % radix); //input/divisor will always result in a number smaller than the radix. Take the mod with the radix to get the value of the number at the digit.
+	
+	return retVal;
+	
+}
