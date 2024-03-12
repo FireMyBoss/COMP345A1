293eed1ba7e183419e902e2775322e6d92d6e528
shaiTroy <shaitroy6@gmail.com>
2024-03-11 20:57:58 -0400

    added func

--- Source_Files_Trial_3/MapCreator.cpp
+++ Source_Files_Trial_3/MapCreator.cpp
@@ -5,6 +5,12 @@ using namespace std;
 MapCreator::MapCreator(int width, int height){
     theMap->height = height;
     theMap->width = width;
+
+    
+
+}
+
+void MapCreator::createStart(){
     while (true){
         cout << "Would you like to:\n1. Create a start point at a specific spot\n2. Create a random start point\n\nPlease input one of the numbers above: ";
         int selection;
@@ -15,30 +21,35 @@ MapCreator::MapCreator(int width, int height){
                 int side;
                 cin >> side;
                 if (side == 1 || side == 4){ //top / bottom
-                    theMap->startX = side == 1 ? 0 : width-1;
+                    theMap->startX = side == 1 ? 0 : theMap->width-1;
                     while (true){
-                        cout << "\n\nPlease input a number for the starting index between 0 and " << height-1;
-                        
-                    }
-                    
+                        cout << "\n\nPlease input a number for the starting index between 0 and " << theMap->height-1;
+                        int point;
+                        cin >> point;
+                        if(point > 0 && point < theMap->height-1){
+                            theMap->startY = point;
+                            break;
+                        }
+                        cout << "\n\nIncorrect input. Please try again.\n\n";
+                    }   
                     break;
                 } else if (side == 2 || side == 3){ //left / right
-                    theMap->startY = side == 2 ? 0 : height-1;
+                    theMap->startY = side == 2 ? 0 : theMap->height-1;
                     while (true){
-                        cout << "\n\nPlease input a number for the starting index between 0 and " << width-1;
-                    }
-                    
+                        cout << "\n\nPlease input a number for the starting index between 0 and " << theMap->width-1;
+                        int point;
+                        cin >> point;
+                        if(point > 0 && point < theMap->width-1){
+                            theMap->startX = point;
+                            break;
+                        }
+                        cout << "\n\nIncorrect input. Please try again.\n\n";
+                    } 
                     break;
                 } else {
                     cout << "\n\nIncorrect input. Please try again.\n\n";
                 }
-                
-                
-                
-                
             }
-            
-            
             break;
         } else if (selection == 2){
             theMap->createStart();
@@ -47,10 +58,6 @@ MapCreator::MapCreator(int width, int height){
         } else {
             cout << "\n\nIncorrect input. Please try again.\n\n";
         }
-    
-    
     }
-    
-    
 }
 
